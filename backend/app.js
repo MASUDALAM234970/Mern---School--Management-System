ed2af66d8fca233524e31b254b4b56da0323f67e
mdmas <mdmasudalam2349@gmail.com>
2025-02-06 21:42:19 +0600

    Add announcement and library management functionality with create and retrieve endpoints; implement corresponding routers and controllers

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,9 @@ import studentRouter from "./router/studentRouter.js";
 //import teacherRouter from "./router/teacherRouter.js";
 //import assignmentRouter from "./router/assignmentRouter.js";
 
-//import announcementRouter from "./router/announcementRouter.js";
+import announcementRouter from "./router/announcementRouter.js";
 //import classRouter from "./router/classRouter.js";
-//import libraryRouter from "./router/libraryRouter.js";
+import libraryRouter from "./router/libraryRouter.js";
 import eventsRouter from "./router/eventsRouter.js";
 //import examRouter from "./router/examRouter.js";
 //import attendanceRouter from "./router/attendanceRouter.js";
@@ -24,6 +24,20 @@ app.use(express.urlencoded({ extended: true })); // ✅ (Optional) Middleware fo
 
 app.use("/api/v1/students", studentRouter);
 app.use("/api/v1/events", eventsRouter);
+app.use("/api/v1/library", libraryRouter);
+
+//app.use("/api/v1/teachers", teacherRouter);
+//app.use("/api/v1/assignments", assignmentRouter);
+
+app.use("/api/v1/announcements", announcementRouter);
+//app.use("/api/v1/class", classRouter);
+//app.use("/api/v1/library", libraryRouter);
+//app.use("/api/v1/events", eventsRouter);
+//app.use("/api/v1/exam", examRouter);
+//app.use("/api/v1/attendance", attendanceRouter);
+
+//app.use("/api/v1/users", usersRouter);
+//app.use("/api/v1/register", adminRegisterRouter);
 
 // ✅ Global Error Handler Middleware (MUST be placed at the end)
 app.use(errorHandler);
