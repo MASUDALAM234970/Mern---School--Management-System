b69b43265c10c0663d46f5417ad6835650c9e892
mdmas <mdmasudalam2349@gmail.com>
2025-02-06 21:49:36 +0600

    Refactor routing structure: remove announcement router, add assignment controller with create and retrieve functionality

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,14 @@ import studentRouter from "./router/studentRouter.js";
 //import teacherRouter from "./router/teacherRouter.js";
 //import assignmentRouter from "./router/assignmentRouter.js";
 
-import announcementRouter from "./router/announcementRouter.js";
+//import announcementRouter from "./router/announcementRouter.js";
 //import classRouter from "./router/classRouter.js";
 import libraryRouter from "./router/libraryRouter.js";
 import eventsRouter from "./router/eventsRouter.js";
 //import examRouter from "./router/examRouter.js";
 //import attendanceRouter from "./router/attendanceRouter.js";
 //import usersRouter from "./router/usersRouter.js";
-//import adminRegisterRouter from "./router/adminRegisterRouter.js";
+//import adminRegisterRouter from "./router/";
 import { errorHandler } from "./middlewares/errorHandler.js"; // ✅ Import error handler
 
 const app = express();
@@ -24,14 +24,13 @@ app.use(express.urlencoded({ extended: true })); // ✅ (Optional) Middleware fo
 
 app.use("/api/v1/students", studentRouter);
 app.use("/api/v1/events", eventsRouter);
-app.use("/api/v1/library", libraryRouter);
 
 //app.use("/api/v1/teachers", teacherRouter);
 //app.use("/api/v1/assignments", assignmentRouter);
 
-app.use("/api/v1/announcements", announcementRouter);
+//app.use("/api/v1/announcements", announcementRouter);
 //app.use("/api/v1/class", classRouter);
-//app.use("/api/v1/library", libraryRouter);
+app.use("/api/v1/library", libraryRouter);
 //app.use("/api/v1/events", eventsRouter);
 //app.use("/api/v1/exam", examRouter);
 //app.use("/api/v1/attendance", attendanceRouter);
